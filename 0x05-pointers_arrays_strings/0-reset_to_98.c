9aa9cae7ff50166cc9d84de0db446b8f469a4806
Nabil Mkadmi <nabilmkadmi@gmail.com>
2023-10-16 18:10:58 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * reset_to_98 - reset the number to 98
- * @n: pointer to the number to reset
- *
- * Return: Always 0.
- */
+* reset_to_98 - reset the number to 98
+* @n: pointer to the number to reset
+*
+* Return: Always 0.
+*/
 int reset_to_98(int *n)
 {
-    *n = 98;
-    return (0);
+	*n = 98;
+	return (0);
 }
