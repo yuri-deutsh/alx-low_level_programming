29ae3cbae33835bab01ebaea4ad35741fd8d3049
Nabil Mkadmi <nabilmkadmi@gmail.com>
2023-10-16 19:42:19 -0700

    _.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* rev_string - Reverses a string
+* @s: Input string
+* String in reverse
+*/
+void rev_string(char *s)
+{
+	int length = 0;
+	int start = 0;
+	int end = 0;
+	char temp;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	end = length - 1;
+	while (start < end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
+}
