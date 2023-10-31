79ce54d7490177daaab93923ae74f0a6e97c1f45
Nabil Mkadmi <nabilmkadmi@gmail.com>
2023-10-31 15:59:04 -0700

    _malloc

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdlib.h>
+
+void util(char **, char *);
+void create_word(char **, char *, int, int, int);
+
+/**
+ * strtow - Splits a string into words.
+ * @str: The string
+ *
+ * Return: Returns a pointer to an array of strings (words).
+ */
+char **strtow(char *str)
+{
+	char **words;
+	int i, flag, len;
+
+	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
+		return (NULL);
+
+	i = flag = len = 0;
+	while (str[i])
+	{
+		if (flag == 0 && str[i] != ' ')
+			flag = 1;
+		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+		{
+			flag = 0;
+			len++;
+		}
+		i++;
+	}
+
+	len += flag == 1 ? 1 : 0;
+	if (len == 0)
+		return (NULL);
+
+	words = (char **)malloc(sizeof(char *) * (len + 1));
+	if (words == NULL)
+		return (NULL);
+
+	util(words, str);
+	words[len] = NULL;
+	return (words);
+}
+
+/**
+ * util - A utility function for fetching words into an array.
+ * @words: The strings array
+ * @str: The string
+ */
+void util(char **words, char *str)
+{
+	int i, j, start, flag;
+
+	i = j = flag = 0;
+	while (str[i])
+	{
+		if (flag == 0 && str[i] != ' ')
+		{
+			start = i;
+			flag = 1;
+		}
+
