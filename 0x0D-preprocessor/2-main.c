fcf29e2ad9e2eea6f9a7bdb602b132aadc2d03ad
Nabil Mkadmi <nabilmkadmi@gmail.com>
2023-11-05 16:13:20 -0800

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the name of the source file.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
