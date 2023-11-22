d0ed1f272338bfad22b10b68e43532bdb31b3a9c
nabil mkadmi <nabilmk120@gmail.com>
2023-11-22 13:46:10 -0800

    2sd

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,26 +1,26 @@
 #include "lists.h"
+
 /**
- * pop_listint - deletes the head node of a listint_t linked list
- * @head: double pointer to the head of the list
+ * pop_listint - Deletes the head node of a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
  *
- * Return: the head nodes data (n)
+ * Return: If the linked list is empty - 0.
+ *         Otherwise - The head node's data (n).
  */
 int pop_listint(listint_t **head)
 {
-	int n = 0;
-	listint_t *ptraux;
-	/* task condition */
+	listint_t *tmp;
+	int ret;
+
 	if (*head == NULL)
 		return (0);
-	/* n is equal to the head de reference to n from the structure */
-	/* that is the nodes data (n) that are integers */
-	n = (*head)->n;
-	/* head goes to the next node */
-	ptraux = (*head)->next;
-	/* frees head so the head node is deleted */
-	free(*head);
-	/* now ptraux is the new head node */
-	*head = ptraux;
-	/* return the head nodes data that is n according to the structure */
-	return (n);
+
+	tmp = *head;
+	ret = (*head)->n;
+	*head = (*head)->next;
+
+	free(tmp);
+
+	return (ret);
 }
