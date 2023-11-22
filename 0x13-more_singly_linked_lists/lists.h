6efe05cb5871bb528f7eabc20121f9409504969a
nabil mkadmi <nabilmk120@gmail.com>
2023-11-21 16:52:48 -0800

    102

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,7 +5,7 @@
  * File: lists.h
  * Auth: Mk
  * Desc: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x12-more_singly_linked_lists directory.
+ *       and types written in the 0x13-more_singly_linked_lists directory.
  */
 
 #include <stdlib.h>
