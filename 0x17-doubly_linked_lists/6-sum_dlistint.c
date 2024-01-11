ef940a6ecb88c550239e8e7d486182ab41566dd1
nabil <nabilmkadmi@gmail.com>
2024-01-11 10:18:12 -0800

    c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -5,14 +5,15 @@
  * @head: list
  * Return: int
  */
-
 int sum_dlistint(dlistint_t *head)
 {
-	int sum = 0;
-	while (head != NULL)
-	{
-		sum += head->n;
-		head = head->next;
-	}
-	return (sum);
+    int sum = 0;
+
+    while (head != NULL)
+    {
+        sum += head->n;
+        head = head->next;
+    }
+
+    return (sum);
 }
