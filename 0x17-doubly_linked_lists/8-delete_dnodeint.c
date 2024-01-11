2073934c6e86254ba9236f8b7bd777ae088d183b
nabil <nabilmkadmi@gmail.com>
2024-01-11 10:25:35 -0800

    c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -3,8 +3,8 @@
 
 /**
  * delete_dnodeint_at_index - delete node at give index
- * @head:list
- * @index:given index
+ * @head: list
+ * @index: given index
  * Return: -1 or 0
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
@@ -12,11 +12,13 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *start;
 	unsigned int i;
 	unsigned int len;
-	len = len_node(&head);
+
+	len = len_node(head);
 
 	start = *head;
 	if (*head == NULL)
 		return (-1);
+
 	if (index == 0)
 	{
 		start = start->next;
@@ -26,18 +28,21 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			start->prev = NULL;
 		return (1);
 	}
+
 	for (i = 0; i <= index - 1; i++)
 	{
 		start = start->next;
 		if (!start)
 			return (-1);
 	}
+
 	if (len - 1 == index)
 	{
 		start->prev->next = NULL;
 		free(start);
 		return (1);
 	}
+
 	start->prev->next = start->next;
 	start->next->prev = start->prev;
 	free(start);
@@ -47,8 +52,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 /**
  * len_node - list len
  *
- * @node:list
- * Return:unsigned int
+ * @node: list
+ * Return: unsigned int
  */
 unsigned int len_node(dlistint_t **node)
 {
